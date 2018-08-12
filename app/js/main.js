be548033319186eecf815c9d579e8f1cb8468b57
dmitriy <dmitriy1a22222@gmail.com>
2018-08-12 15:48:34 +0300

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -4,18 +4,18 @@ class Talk {
         block.addEventListener('click', () => {
             //this.defineProperty
 
-            if(this.flag) {
-                this.flag = false;
+            if(this.flag === false) {
+                this.flag = true;
                 arrow.style.cssText = 'transform: rotate(180deg)';
                 descriptionBlock.style.cssText = 'display: block';
 
 
             } else {
-                this.flag = true;
+                this.flag = false;
                 arrow.style.cssText = 'transform: rotate(0)';
                 descriptionBlock.style.cssText = 'display: none';
-                console.log(this.flag);
             }
+            console.log(this.flag);
 
         });
     }
@@ -25,5 +25,17 @@ let block1 = document.querySelectorAll('.our-talk__item')[0];
 let arrow1 = document.querySelectorAll('.our-talk__item_arrow')[0];
 let descriptionBlock1 = document.querySelectorAll('.our-talk__description-text')[0];
 
-let talk = new Talk(block1, arrow1, descriptionBlock1);
+let talk1 = new Talk(block1, arrow1, descriptionBlock1);
+
+let block2 = document.querySelectorAll('.our-talk__item')[1];
+let arrow2 = document.querySelectorAll('.our-talk__item_arrow')[1];
+let descriptionBlock2 = document.querySelectorAll('.our-talk__description-text')[1];
+
+let talk2 = new Talk(block2, arrow2, descriptionBlock2);
+
+let block3 = document.querySelectorAll('.our-talk__item')[2];
+let arrow3 = document.querySelectorAll('.our-talk__item_arrow')[2];
+let descriptionBlock3 = document.querySelectorAll('.our-talk__description-text')[2];
+
+let talk3 = new Talk(block3, arrow3, descriptionBlock3);
 
