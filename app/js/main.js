1f09644e46aad65a165a8a7677eaeb29a0b8d92f
dmitriy <dmitriy1a22222@gmail.com>
2018-08-21 14:55:30 +0300

    last commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+let wow = new WOW(
+    {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+);
+
 
 class Talk {
     constructor(items) {
@@ -16,8 +31,7 @@ class Talk {
     }
 }
 document.addEventListener('DOMContentLoaded', () => {
-    let talk = new Talk('our-talk__item');
-    talk.logicAccordion();
+
 });
 
 
@@ -69,10 +83,32 @@ class Slider {
     }
 }
 
+class NavToggler {
+    constructor(burger, navList) {
+        this.burger = document.querySelector(`.${burger}`);
+        this.navList = document.querySelector(`.${navList}`);
+    }
+    togglerLogic() {
+        this.burger.addEventListener('click', () => {
+            this.burger.classList.toggle('active');
+            this.navList.classList.toggle('active')
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    wow.init();
+
+    let talk = new Talk('our-talk__item');
+    talk.logicAccordion();
+
     let slider1 = new Slider('slider-wrap-item', 'slider-item', 'arrow-prev__wrap', 'arrow-next__wrap');
     slider1.sliderLogic();
 
     let slider2 = new Slider('slider-wrap-item-work', 'slider-item-work', 'arrow-prev__wrap-work', 'arrow-next__wrap-work');
     slider2.sliderLogic();
+
+    let navToggler = new NavToggler('burger', 'nav__list');
+    navToggler.togglerLogic()
 });
+
